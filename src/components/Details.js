49fb6c4384b79921e6ea949f63c52ba0c786faca
HassenH1 <hasansaid51@gmail.com>
2020-07-06 14:57:11 -0700

    mock data for now

--- src/components/Details.js
+++ src/components/Details.js
@@ -8,7 +8,46 @@ export default class Details extends Component {
     return (
       <ProductConsumer>
         {(value) => {
-          console.log(value.detailProducts);
+          const {
+            id,
+            company,
+            title,
+            img,
+            price,
+            info,
+            inCart,
+          } = value.detailProduct;
+
+          return (
+            <div className="container py-5">
+              {/* title */}
+              <div className="row">
+                <div className="col-10 mx-auto text-center text-slanted text-blue my-5">
+                  <h1>{title}</h1>
+                </div>
+              </div>
+              {/* end of title */}
+              {/* product info */}
+              <div className="row">
+                <div className="col-10 mx-auto col-md-6 my-3">
+                  <img src={img} alt="product" className="img-fluid" />
+                </div>
+                {/* product text */}
+                <div className="col-10 mx-auto col-md-6 my-3 text-capitalize">
+                  <h1>Model: {title}</h1>
+                  <h4 className="text-title text-uppercase text-muted mt-3 mb-2">
+                    Made by: <span className="text-uppercase">{company}</span>
+                  </h4>
+                  <h4 className="text-blue">
+                    <strong>
+                      Price: <span>$</span>
+                      {price}
+                    </strong>
+                  </h4>
+                </div>
+              </div>
+            </div>
+          );
         }}
       </ProductConsumer>
     );
