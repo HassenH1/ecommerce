9295a29cab3c2562f7b40eb7a30d76c5fcba1c4c
HassenH1 <hasansaid51@gmail.com>
2020-07-06 10:52:46 -0700

    in Cart butto

--- src/components/Product.js
+++ src/components/Product.js
@@ -7,7 +7,7 @@ export default class Product extends Component {
   render() {
     const { id, title, img, price, inCart } = this.props.product;
     return (
-      <ProductWrapper className="col-9 mx-auto col-md-6 col-lg-3">
+      <ProductWrapper className="col-9 mx-auto col-md-6 col-lg-3 my-3">
         <div className="card">
           <div
             className="img-container p-5"
@@ -16,6 +16,21 @@ export default class Product extends Component {
             <Link to="/details">
               <img src={img} alt="product" className="card-img-top" />
             </Link>
+            <button
+              className="cart-btn"
+              disabled={inCart ? true : false}
+              onClick={() => {
+                console.log("add to cart");
+              }}
+            >
+              {inCart ? (
+                <p className="text-capitalize mb-0" disabled>
+                  In Cart
+                </p>
+              ) : (
+                <i className="fas fa-cart-plus" />
+              )}
+            </button>
           </div>
         </div>
       </ProductWrapper>
@@ -23,6 +38,4 @@ export default class Product extends Component {
   }
 }
 
-const ProductWrapper = styled.div`
-  .img-container: ;
-`;
+const ProductWrapper = styled.div``;
