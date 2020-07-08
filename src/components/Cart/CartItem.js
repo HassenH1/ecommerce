99d467efc2bbffd36ace7879a264647a806aeb54
HassenH1 <hasansaid51@gmail.com>
2020-07-07 21:39:17 -0700

    cart almost there

--- src/components/Cart/CartItem.js
+++ src/components/Cart/CartItem.js
@@ -23,6 +23,21 @@ function CartItem({ item, value }) {
         <span className="d-lg-none">Price:</span>
         {price}
       </div>
+
+      <div className="col-10 mx-auto col-lg-2 my-2 my-lg-0">
+        <div className="d-flex justift-content-center">
+          <div className="">
+            <span className="btn btn-black mx-1" onClick={() => decrement(id)}>
+              -
+            </span>
+            <span className="btn btn-black mx-1">{count}</span>
+            <span className="btn btn-black mx-1" onClick={() => increment(id)}>
+              +
+            </span>
+          </div>
+        </div>
+      </div>
+      {/*  */}
     </div>
   );
 }
