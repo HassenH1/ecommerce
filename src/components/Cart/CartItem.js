6c9f1d76e638520c40e661a0c9969f077960273c
HassenH1 <hasansaid51@gmail.com>
2020-07-07 21:30:10 -0700

    product name and price and image

--- src/components/Cart/CartItem.js
+++ src/components/Cart/CartItem.js
@@ -1,7 +1,30 @@
 import React from "react";
 
 function CartItem({ item, value }) {
-  return <div>C</div>;
+  const { id, title, img, price, total, count } = item;
+  const { increment, decrement, removeItem } = value;
+  return (
+    <div className="row my-1 text-capitalize text-center">
+      <div className="col-10 mx-auto col-lg-2">
+        <img
+          src={img}
+          style={{ width: "5rem", height: "5rem" }}
+          className="img-fluid"
+          alt="product"
+        />
+      </div>
+
+      <div className="col-10 mx-auto col-lg-2">
+        <span className="d-lg-none">Product:</span>
+        {title}
+      </div>
+
+      <div className="col-10 mx-auto col-lg-2">
+        <span className="d-lg-none">Price:</span>
+        {price}
+      </div>
+    </div>
+  );
 }
 
 export default CartItem;
